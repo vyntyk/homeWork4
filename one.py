86575d552ceb0474d1c4d2304ef5ffb8a97ed149
Viktor Belous <vyntyk@yandex.ru>
2022-10-04 21:04:23 +0300

    init

---
+++ one.py
@@ -0,0 +1,7 @@
+# 1. Вычислить число c заданной точностью d
+# in # Enter a real number: 9
+# Enter the required accuracy '0.0001': 0.000001
+# out 9.000000
+# in Enter a real number: 8.98785
+# Enter the required accuracy '0.0001': 0.001
+# out 8.988
